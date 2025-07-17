54585c161b380ca611472c4ca2adeae8f23bdf51
Gianni Gabriel <clondireanug@gmail.com>
2025-07-17 22:13:13 +0200

    improved layout of home page and created a header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import {Routes, Route} from 'react-router-dom'
 import { HomePage } from './pages/home.jsx'
 import { PlayPage } from './pages/playPage.jsx'
 import { LeaderboardPage } from './pages/leaderboardPage.jsx'
+import { Header } from './components/header.jsx'
 
 export function App() {
 
   return (
     <>
+      <Header/>
       <main>
         <Routes>
           <Route path="/" element={<HomePage/>} />
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,17 @@
+import "/styles/header.css";
+export function Header() {
+  return (
+    <header>
+      <h1>
+        Sp
+        <span>
+          <img
+            src="../assets/crosshair.png"
+            alt="image of a corsshair simuling an O"
+          />
+        </span>
+        t <span className="itText">IT</span>
+      </h1>
+    </header>
+  );
+}
