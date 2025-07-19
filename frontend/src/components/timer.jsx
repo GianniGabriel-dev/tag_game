901428170c1cb2231d63caa131c143b107b7fde1
Gianni Gabriel <clondireanug@gmail.com>
2025-07-19 23:07:41 +0200

    feat:created timer for the game

---
+++ frontend/src/components/timer.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export function Timer({ initialMinutes = 0, initialSeconds = 0 }) {
+  const [totalTime, setTotalTime] = useState(initialMinutes * 60 + initialSeconds);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTotalTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(interval); 
+  }, []);
+
+  const minutes = Math.floor(totalTime / 60);
+  const seconds = totalTime % 60;
+
+  return (
+    <div className="timer">
+      <h2>
+        {/*pad start asegura que se muestren siempre dos digitos 00:00*/}
+        {String(minutes).padStart(2, "0")} : {String(seconds).padStart(2, "0")}
+      </h2>
+    </div>
+  );
+}
--- frontend/src/pages/playPage.jsx
+++ frontend/src/pages/playPage.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom"
 import "/styles/playPage.css"
 import { useState } from "react";
 import { imagesToFind } from "../../utils/ArrayImages.js";
+import { Timer } from "../components/timer.jsx";
+
 
 export function PlayPage() {
   const {selectedGame} = useParams();
@@ -13,21 +15,44 @@ export function PlayPage() {
   console.log(gameData.characters[0].img)
   const [message, setMessage]= useState("")
 
+ const handleImageClick = (event) => {
+    const img = event.target;
+    
+    const rect = img.getBoundingClientRect(); // obtine el tamaño y la posicion de la imagen dependiendo del tamaño de la pantalla
+
+    //se calcula las coordenadas del click en la imagen
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    
+    //se pasa las coordenads a porcentajes para hacer que los ckicks sean responsivos a cualquier tamaño de pantalla
+    const percentX = (x / rect.width) * 100;
+    const percentY = (y / rect.height) * 100;
+
+    const formattedX = percentX.toFixed(2);
+    const formattedY = percentY.toFixed(2);
+
+    setMessage(`Has hecho clic en: ${formattedX}% X, ${formattedY}% Y`);
+  };
+
   if(!gameData) return <p>we can't find that game</p>
 
   return (
     <>
         <section className="gameMenu">
             <h2>{formatedSelectedGame}🎮</h2>
-            <article className="charactersContainer">
-              <ul>
-                <li><p>{gameData.characters[0].name }</p><img src={`characters/${gameData.characters[0].img}`}/></li>
-                <li><p>{gameData.characters[1].name }</p><img src={`characters/${gameData.characters[1].img}`}/></li>
-                <li><p>{gameData.characters[2].name }</p><img src={`characters/${gameData.characters[2].img}`}/></li>
-              </ul>
-              <p className="message">{message}</p>
-            </article>
-            <img src={`/${selectedGame}.png`} alt={`Big image of a ${selectedGame}`} />
+            <section className="stickyMenus">
+              <article className="charactersContainer">
+                <ul>
+                  <li><p>{gameData.characters[0].name }</p><img src={`characters/${gameData.characters[0].img}`}/></li>
+                  <li><p>{gameData.characters[1].name }</p><img src={`characters/${gameData.characters[1].img}`}/></li>
+                  <li><p>{gameData.characters[2].name }</p><img src={`characters/${gameData.characters[2].img}`}/></li>
+                </ul>
+                <p className="message">{message}</p>
+              </article>
+              <Timer/>
+            </section>
+
+            <img onClick={handleImageClick} src={`/${selectedGame}.png`} alt={`Big image of a ${selectedGame}`} />
         </section>
         
     </>
